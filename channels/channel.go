945f97cea1124d19f45f9418b420a3828a6a441c
Opeyemi Popoola <popstarbaed@gmail.com>
2020-08-21 20:02:12 +0100

    added comments

--- channels/channel.go
+++ channels/channel.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-//MakeArr...
+// MakeArr...
 func MakeArr(len int) (arr []int) {
 	var a int
 	for a = 0; a < len; a++ {
@@ -19,11 +19,11 @@ func summer(num int, result chan int) {
 	var total int
 
 	var i int
-
+	// returns the sum of the array
 	for i = 0; i < num; i++ {
 		total += i
 	}
-
+	// returning value to channel
 	result <- total
 }
 
@@ -35,6 +35,7 @@ func looper(number int) (result []int, sumChan int) {
 	chanresult := make(chan int, 1)
 	var acc int
 
+	// creating channels to calculate the total sum of each array value
 	for acc = 0; acc < len(x); acc++ {
 		go summer(x[acc], chanresult)
 		x := <-chanresult
@@ -45,6 +46,7 @@ func looper(number int) (result []int, sumChan int) {
 	return
 }
 
+//TestChannel...
 func TestChannel(testNumber int) {
 	start := time.Now()
 	_, sumOfArray := looper(testNumber)
